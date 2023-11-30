676bab5e556ada246bee760b34988df5e847908b
Shu87888 <shushant.nimgade0@gmail.com>
2023-11-30 16:14:02 +0530

    check1

--- Sonu_repository.js
+++ Sonu_repository.js
@@ -3,3 +3,20 @@ let arr = [1,4,5,8,5455,505,577,56,85,50,"Sonu Nimagde"];
 for(let i = 0; i < arr.length; i++){
     console.log("arr =",arr[i]);
 }
+
+function functionName(){
+    const userName = "ShuShant G. Nimgade";
+    const passWord = 12345;
+    console.log("userName =",userName);
+    console.log("passWord =",passWord);
+    console.log("------------------------")
+    let name = "sonu n.........";
+    let lastname = "Nimgade";
+    console.log("name =",name);
+    console.log("lastname =",lastname);
+
+}
+
+functionName();
+
+
